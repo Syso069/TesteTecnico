329d2db14666c23c4587a1d408a32de482a40c45
Syso069 <sisomd69@gmail.com>
2024-09-23 19:49:47 -0300

    commit

--- questao1/src/Main.java
+++ questao1/src/Main.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        1) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores
-//        anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informando um número,
-//        ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
-
         System.out.println("Digite um número:\n");
 
         Scanner numeroDigitado = new Scanner(System.in);
